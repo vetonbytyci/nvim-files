225d0f97ae46d7c8b0a6c854408852a525844047
Veton Bytyçi <vetonbytyci@Vetons-MacBook-Pro-2.local>
2023-01-26 01:39:49 +0100

    Changed color for lspsaga window

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -1,7 +1,41 @@
 local status, saga = pcall(require, "lspsaga")
 if (not status) then return end
 
-saga.setup({})
+saga.setup({
+  ui = {
+    -- currently only round theme
+    theme = 'round',
+    -- this option only work in neovim 0.9
+    title = true,
+    -- border type can be single,double,rounded,solid,shadow.
+    border = 'solid',
+    winblend = 1,
+    expand = '',
+    collapse = '',
+    preview = ' ',
+    code_action = '💡',
+    diagnostic = '🐞',
+    incoming = ' ',
+    outgoing = ' ',
+    colors = {
+      --float window normal background color
+      normal_bg = '#1c1c19',
+      --title background color
+      title_bg = '#afd700',
+      red = '#e95678',
+      magenta = '#b33076',
+      orange = '#FF8700',
+      yellow = '#f7bb3b',
+      green = '#afd700',
+      cyan = '#36d0e0',
+      blue = '#61afef',
+      purple = '#CBA6F7',
+      white = '#d1d4cf',
+      black = '#1c1c19',
+    },
+    kind = {},
+  },
+})
 
 local keymap = vim.keymap.set
 -- Lsp finder find the symbol definition implement reference
