f8d7c434c282ed1a7cbcb93cbabcbb2ee99b6cc3
vetonbytyci <vetonbb@gmail.com>
2023-04-21 14:46:36 +0200

    feat(bufferline): default theme color for vim tabs

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -8,40 +8,9 @@ bufferline.setup({
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    color_icons = true
+    color_icons = true,
   },
-  highlights = {
-    tab = {
-      fg = '#657b83',
-      bg = '#ffffff',
-    },
-    separator = {
-      fg = '#657b83',
-      bg = '#000000',
-    },
-    separator_selected = {
-      fg = '#073642',
-      bg = '#000000',
-    },
-    background = {
-      fg = '#657b83',
-      bg = '#000000',
-      opacity = 0.5,
-    },
-    buffer_selected = {
-      fg = '#fdf6e3',
-      bold = true,
-      bg = '#000000',
-    },
-    buffer_visible = {
-      bg = '#000000',
-      fg = '#fdf6e3',
-    },
-    fill = {
-      bg = '#000000',
-      opacity = 0.5,
-    }
-  },
+  highlights = {},
 })
 
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
